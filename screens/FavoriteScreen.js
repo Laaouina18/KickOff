2ed99d1e33f0a6d591974e3097b5cfc4eee3fca0
Laaouina18 <laaouinanouhaila2019@gmail.com>
2024-02-27 06:49:36 +0100

    KIC-5 creer le screen des favorites matches

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -4,12 +4,24 @@ import MatchItem from '../components/MatchItem'
 import { useSelector } from 'react-redux'
 
 
-export default function Favorites() {
-    const favorites =  useSelector(state => state.favorites)
+export default function Favoris() {
+    const favoris =  useSelector(state => state.favorites)
 
 return (
     <>
-   <Text>lkflkf</Text>
+    {
+            favoris.length > 0 ? 
+     (<View style={{ marginTop: 20 }}>
+                <FlatList
+                    data={favoris}
+                    renderItem={({ item }) => {
+                        return <MatchItem match={item} />
+                    }}
+                    ItemSeparatorComponent={() => <View style={{ height: 30 }} />}
+                    keyExtractor={item => item.id}
+                />
+            </View>) : (<Text style={{ textAlign:'center', marginTop:120 , fontSize:30}}> NO favorited posts</Text>)
+    }
     </>
 )
 }
